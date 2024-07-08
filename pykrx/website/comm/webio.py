5d75cdca337ce3a60385dfbdbb116234d91f0b80
노영빈 <yb.noh@phase3.team>
2024-07-08 18:34:24 +0900

    Request Header 변경

--- pykrx/website/comm/webio.py
+++ pykrx/website/comm/webio.py
@@ -4,7 +4,12 @@ from abc import abstractmethod
 
 class Get:
     def __init__(self):
-        self.headers = {"User-Agent": "Mozilla/5.0"}
+        self.headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Host": "data.krx.co.kr",
+            "Origin": "http://data.krx.co.kr",
+            "Referer": "http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201"
+        }
 
     def read(self, **params):
         resp = requests.get(self.url, headers=self.headers, params=params)
@@ -18,7 +23,12 @@ class Get:
 
 class Post:
     def __init__(self, headers=None):
-        self.headers = {"User-Agent": "Mozilla/5.0"}
+        self.headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Host": "data.krx.co.kr",
+            "Origin": "http://data.krx.co.kr",
+            "Referer": "http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201"
+        }
         if headers is not None:
             self.headers.update(headers)
 
